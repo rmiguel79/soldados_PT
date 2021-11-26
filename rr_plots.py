4b3a9997b29c9765f03b977a54d5c80cc9ed2dc5
rmiguel79 <38146772+rmiguel79@users.noreply.github.com>
2021-11-26 23:05:11 +0000

    Estado inicial
    Limpei algum código no rr_projecoes e puz uns comentários.
    Criei o rr_plots para usar esses dados para criar uns gráficos.

---
+++ rr_plots.py
@@ -0,0 +1,134 @@
+# :tabSize=2:indentSize=2:noTabs=false:
+'''Cria plots offline de velocidade e de progresso diário de RR do clã.
+As velocidades são "normalizadas", ou seja, é descontado o efeito dos bónus diários
+para se poder comparar efetivamente o "quanto se vence e participa" diariamente entre
+dias e semanas diferentes.
+'''
+from plotly.offline import plot
+import numpy as np
+import plotly.graph_objs as go
+
+from rr_projecoes import pesos, vals
+
+# Objetivo definido para cada semana.
+lims = [20, 30, 65, 92, 65, 92]
+for _ in range(len(lims), len(vals)):
+	lims.append(65)
+
+data = []
+velocidades_ys = []
+for n,(semana,lim) in enumerate(zip(vals, lims)):
+	semana_xs, semana_ys = [],[]
+	for x,y in semana:
+		semana_xs.append(x)
+		semana_ys.append(y)
+	
+	semana_xs.append(semana_xs[-1]+1)
+	semana_ys.append(lim*1000)
+	
+	velocidade_ys = []
+	prv = 0.
+	for a,b in zip(semana_ys, pesos):
+		# Ao dividir pelo bónus diário, em "b", estamos a reduzir todos os valores à mesma
+		# escala, como se não houvesse bónus diários, para se poder comparar velocidades
+		# entre dias diferentes da semana.
+		velocidade_ys.append((a-prv) / b)
+		prv = a
+	velocidades_ys.append(velocidade_ys[:-1])
+	
+	# Valores de RR diários (a acumular ao longo dos dias em cada semana).
+	data.append(go.Scatter(
+	  x=np.array(semana_xs) + (n*7),
+	  y=np.array(semana_ys),
+	  marker=dict(size=8),
+	  mode='lines+markers',
+	  name=f'RR na semana {n+1}',
+	  text=(f'RR na semana {n+1}',),
+	  hoverinfo='text',
+	  visible=True,
+	  ))
+	
+	# Velocidade diária, descartando bónus diários.
+	data.append(go.Scatter(
+	  x=np.array(semana_xs) + (n*7),
+	  y=np.array(velocidade_ys),
+	  marker=dict(size=8),
+	  mode='lines+markers',
+	  name=f'semana {n+1}: velocidade',
+	  text=(f'semana {n+1}: velocidade',),
+	  hoverinfo='text',
+	  visible=False,
+	  ))
+
+# Para cada semana, calcula-se a velocidade média e faz-se um plot dela, para vermos
+# como é que o clã vai evoluindo ao longo de semanas sucessivas.
+medias_semanais = [sum(a) / len(a)  for a in velocidades_ys]
+data.append(go.Scatter(
+  x=np.arange(len(medias_semanais)) * 7. + 3.5,
+  y=np.array(medias_semanais),
+  marker=dict(size=8),
+  mode='lines+markers',
+  name=f'Evolução veloc.',
+  text=(f'Evolução veloc.',),
+  hoverinfo='text',
+  visible=False,
+  ))
+
+layout = dict(
+  title=dict(text='Conquistas de RR dos Soldados PT', x=0.5),
+  xaxis=dict(title='Dias ao longo dos tempos'),
+  yaxis=dict(title='RR'),
+  updatemenus=[dict(
+    active=0,
+    bordercolor='#BEC8D9',
+    buttons=[
+      dict(
+        args=[dict(visible=sum([[True,False] for _ in vals], []) + [False])],
+        label='RR acumulados',
+        method='update',),
+      dict(
+        args=[dict(visible=sum([[False,True] for _ in vals], []) + [False])],
+        label='Velocidades diárias',
+        method='update',),
+      dict(
+        args=[dict(visible=sum([[False,True] for _ in vals], []) + [True])],
+        label='Veloc.+evolução',
+        method='update',),
+      dict(
+        args=[dict(visible=sum([[False,False] for _ in vals], []) + [True])],
+        label='Evolução do clã',
+        method='update',),
+      ],
+    direction='down',
+    showactive=True,
+    type='buttons',
+    )]
+  )
+
+
+fig = dict(data=data, layout=layout)
+
+hdr = '''<!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8"/>
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+    </head>
+  <body>
+    '''.replace('\n  ', '\n')
+
+ftr = '''
+    </body>
+  </html>
+  '''.replace('\n  ', '\n')
+
+my_div = plot(fig, include_plotlyjs=False, output_type='div')
+# my_div = my_div.replace(', {"showLink": true, "linkText": "Export to plot.ly"}', '')
+# my_div = my_div.replace(', {"linkText": "Export to plot.ly", "showLink": true}', '')
+# my_div = my_div.replace('style="height: 100%; width: 100%;"', 'style="height: 750px; width: 1500px;"')
+
+with open('rr_plots.html', 'w') as fw:
+	fw.write(hdr)
+	fw.write(my_div)
+	fw.write(ftr)
+print('rr_plots.html escrito.')
---
+++ rr_projecoes.py
@@ -0,0 +1,137 @@
+# :tabSize=2:indentSize=2:noTabs=false:
+discord_old='discord.gg/dHWfrDBnmG'
+discord_old='https://discord.gg/dHWfrDBnmG'
+discord='https://discord.gg/kbA3F8qssg'
+discord='discord.gg/kbA3F8qssg'
+whatsapp='chat.whatsapp.com/BwFpogIrlca6VBSkDYePga'
+lim_sem =65000.
+bonus=(0., 0.2, 0.4, 0.6, 1., 3., 5.)
+pesos=[1.+a for a in bonus]
+
+vals=((
+	(1, 1831.),
+	(2, 4028.),
+	(3, 6592.),
+	(4, 9304.),
+	(5, 12604.),
+	), (
+	(1, 2140.),
+	(2, 5525.),
+	(3, 8715.),
+	(4, 13075.),
+	(5, 19815.),
+	), (
+	(1, 4834.),
+	(2, 10004.),
+	(3, 15229.),
+	(4, 23454.),
+	(5, 32483.),
+	(6, 56635.),
+	),(
+	(1, 4659.),
+	(2, 10961.),
+	(3, 19387.),
+	(4, 28897.),
+	(5, 39923.),
+	(6, 59803.),
+	),(
+	(1, 7698.),
+	(2, 15736.),
+	(3, 26607.),
+	(4, 36701.),
+	(5, 50777.),
+	),(
+	(1, 8702.),
+	(2, 16838.),
+	(3, 26453.),
+	(4, 37467.),
+	(5, 48721.),
+	(6, 77231.),
+	),(
+	(1, 8871.),
+	(2, 19180.),
+	(3, 29059.),
+	(4, 39280.),
+	(5, 55526.),
+	),(
+	(1, 9915.),
+	(2, 21813.),
+	(3, 34090.),
+	(4, 45541.),
+	(5, 63057.),
+	),(
+	(1, 9973.),
+	(2, 21140.),
+	(3, 33358.),
+	(4, 48679.),
+	),(
+	(1, 9521.),
+	(2, 19589.),
+	(3, 32667.),
+	(4, 43184.),
+	(5, 61786.),
+	))
+
+if __name__ == '__main__':
+	# Evitar andar a ver todas as semanas até hoje
+	num_semanas_removidas = 0
+	if 1==1:
+		num_semanas_removidas = len(vals) - 1
+		vals = (vals[-1],)
+	
+	# Projeções
+	for n,semana in enumerate(vals):
+		print(f'semana {n + num_semanas_removidas + 1}')
+		for dia, atual in semana:
+			print(f'  projeção ao dia {dia}')
+			atual_por_unidade = atual / sum(pesos[:dia])
+			projs = []
+			for a in range(dia, len(pesos)+1):
+				val = atual_por_unidade * sum(pesos[:a])
+				print(f'    dia {a} - rr esperado {val:5.0f}')
+				projs.append(val)
+	
+	prv_b = projs.pop(0)
+	
+	# minimos diários como meta
+	if 1==0:
+		for a in range(7):
+			print(f'''\
+	Hoje precisávamos ter atingido {lim_sem*sum(pesos[:a+1])/sum(pesos):.0f}'''.replace('\n\t', '\n').strip())
+	
+	# mínimo no dia atual
+	if 1==0:
+		print(f'{lim_sem*sum(pesos[:vals[-1][-1][0]])/sum(pesos)}')
+	
+	# histórico da semana e previsão da meta
+	if 1==1:
+		hist = ''
+		for a in range(len(vals[-1])):
+			hist += f'\n{vals[-1][a][1]:5.0f}({100.*vals[-1][a][1]/(lim_sem*sum(pesos[:a+1])/sum(pesos)):.0f})%'
+		hist += f'\n{lim_sem/1000.:.0f}k'
+		s = f'Histórico da semana:{hist}\n{discord}'
+		print(f'\n{len(s)}')
+		print(s)
+		
+		prv_a,prv_b = 0,prv_b
+		for a,b in zip(range(vals[-1][-1][0]+1, 8), projs):
+			if float(b) > lim_sem:
+				break
+			prv_a,prv_b = a,b
+		inst_meta = vals[-1][-1][0] + (lim_sem - prv_b) / (b - prv_b)
+		dias = '2a 3a 4a 5a 6a sábado domingo'.split()
+		dia_idx = int(inst_meta)
+		horas = ((inst_meta - dia_idx) * 24.) + 9.
+		if horas >= 24.:
+			horas -= 24.
+			dia_idx += 1
+	
+	s = f'''\
+	Hoje prevemos alcançar {projs[0]:.0f} ({int(projs[-1])*100./lim_sem:.0f}% da linha mínima) e chegar à meta ao dia {inst_meta:.1f},{dias[dia_idx]} pelas {horas:.0f}h
+	Info pa novos membros,temos canal d discord
+	{discord}'''.replace('\n\t', '\n').strip()
+	print(f'\n{len(s)}')
+	print(s)
+
+
+
